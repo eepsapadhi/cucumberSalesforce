59e5b7fc8b42515851f9a40e9c1ec28ec26dc1bf
Eepsa Padhi <eepsa.padhi09@gmail.com>
2022-02-22 18:40:08 -0800

    my second commit

--- CucumberTest/src/test/java/runner/TestRunner.java
+++ CucumberTest/src/test/java/runner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = {"src/test/resources/Salesforce.feature"},
-               glue= {"Steps"})
+                  glue= {"Steps"})
 
 
 public class TestRunner extends AbstractTestNGCucumberTests{
